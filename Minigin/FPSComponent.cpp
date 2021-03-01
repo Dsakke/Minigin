edd26ad07137f81f79ae374d0ca17a60d43fbfc1
Sam <sammeke119@live.com>
2021-03-01 09:28:52 +0100

    FPSComponent now throws an error if updated while attached to a GameObject without a text component

--- Minigin/FPSComponent.cpp
+++ Minigin/FPSComponent.cpp
@@ -8,7 +8,14 @@ void dae::FPSComponent::Update()
 	if (auto pOwner = m_pOwner.lock())
 	{
 		auto pTextComponent = pOwner->GetComponent<TextComponent>();
-		pOwner->GetComponent<TextComponent>()->SetText(std::to_string(1 / Time::GetInstance()->GetElapsed()));
+		if (pTextComponent)
+		{
+			pOwner->GetComponent<TextComponent>()->SetText(std::to_string(1 / Time::GetInstance()->GetElapsed()));
+		}
+		else
+		{
+			throw std::runtime_error("FPSComponent is attached to a GameObject without a TextComponent");
+		}
 	}
 }
 
