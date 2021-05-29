342a20cb80faf306de4e34508946bcce78efd3f2
Sam <sammeke119@live.com>
2021-05-29 17:20:24 +0200

    Fixed typo

--- Minigin/Subject.h
+++ Minigin/Subject.h
@@ -14,7 +14,7 @@ namespace dae
 		void RemoveObserver(std::shared_ptr<Observer> pObserver);
 
 	protected:
-		void Notify(const std::shared_ptr<GameObject> pGameObeject, Events event);
+		void Notify(const std::shared_ptr<GameObject> pGameObject, Events event);
 	private:
 		std::vector<std::shared_ptr<Observer>> m_pObservers;
 	};
